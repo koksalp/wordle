19fd2d54d1ed81a432b51527b94e774ad39df7f2
koksalp <72392963+koksalp@users.noreply.github.com>
2023-01-15 08:30:48 +0300

    Delete wordle-last directory

--- wordle-last/helpers/elements.js
+++
@@ -1,5 +0,0 @@
-export const mainDiv = document.querySelector("#main"); 
-export const winnerElement = document.querySelector("#winner-status");  
-export const answerElement = document.querySelector("#answer"); 
-export const messageElement = document.querySelector("#message");  
-export const hrElement = document.querySelector("hr"); 
