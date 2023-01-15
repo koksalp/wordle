209f18d2a9b8eb7a89053cef68147b3ef219afef
koksalp <72392963+koksalp@users.noreply.github.com>
2023-01-15 08:24:51 +0300

    Delete helpers directory

--- helpers/elements.js
+++
@@ -1,5 +0,0 @@
-export const enterButton = document.querySelector("#enter");
-export const deleteButton = document.querySelector("#delete");
-export const rows = document.querySelectorAll(".row"); 
-export const keys = document.querySelectorAll(".letter"); 
-export const mainDiv = document.querySelector("#main"); 
